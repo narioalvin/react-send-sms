c2be1f47d8da2fa69f2d71b2bbb073df4b2dacb6
Alvin Nario <59873164+narioalvin@users.noreply.github.com>
2020-06-24 16:50:22 +0800

    Updates

---
+++ precache-manifest.04127cb49dfa2a2093fd22c811a77fc3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29545f3d6ddaeeb800f69297a5078e10",
+    "url": "/react-send-sms/index.html"
+  },
+  {
+    "revision": "7fecf34e65e9da2497ce",
+    "url": "/react-send-sms/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5ccff81b0c297fc9910e",
+    "url": "/react-send-sms/static/css/main.404b48a9.chunk.css"
+  },
+  {
+    "revision": "7fecf34e65e9da2497ce",
+    "url": "/react-send-sms/static/js/2.70a9179a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-send-sms/static/js/2.70a9179a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ccff81b0c297fc9910e",
+    "url": "/react-send-sms/static/js/main.6d0a2a4d.chunk.js"
+  },
+  {
+    "revision": "a2ac374a590e752135f5",
+    "url": "/react-send-sms/static/js/runtime-main.1147ba7e.js"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/react-send-sms/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "8b4d6e74cffa8b09d37d84376cd22e12",
+    "url": "/react-send-sms/static/media/check.8b4d6e74.svg"
+  },
+  {
+    "revision": "da20cfba1333987c34da57150b9cbbf0",
+    "url": "/react-send-sms/static/media/mail.da20cfba.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-send-sms/precache-manifest.04127cb49dfa2a2093fd22c811a77fc3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-send-sms/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
